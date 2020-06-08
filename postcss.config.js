cf8ee62547b409b61c6541d0f755a1cdde35484f
Pg Gonni <p.gonni@hotmail.com>
2020-06-08 10:51:09 -0400

    added purgeCSS

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,13 @@ module.exports = {
     plugins: [
         require('tailwindcss'),
         require('autoprefixer'),
+        process.env.NODE_ENV === 'production' && require('@fullhuman/postcss-purgecss')({
+            content: [
+                './public/index-justcss.html',
+                './public/index.html',
+                './public/animation.js'
+            ],
+            defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+        })
     ]
 }
